b59156afe16dd8395241ff3d8cbab05807f1fb20
VARSHITJAISWAL <161512294+VARSHITJAISWAL@users.noreply.github.com>
2025-06-11 22:38:15 +0530

    Update comB.jsx

--- comB.jsx
+++ comB.jsx
@@ -1,16 +1,17 @@
 import style from "./ComB.module.css"
 function ComB() {
+    let value=[1,2,3,"/",4,5,6,"x",7,8,9,"-","DEC",".","AC","+"]
     return (
+
+
+
         <div className={style.grid}>
-            <button className={style.btn}>1</button>
-            <button className={style.btn}>2</button>
-            <button className={style.btn}>3</button>
-            <button className={style.btn}>/</button>
-            <button className={style.btn}>4</button>
-            <button className={style.btn}>5</button>
-            <button className={style.btn}>6</button>
-            <button className={style.btn}>x</button> <button className={style.btn}>7</button> <button className={style.btn}>8</button> <button className={style.btn}>9</button> <button className={style.btn}>-</button> <button className={style.btn}>DEC</button> <button className={style.btn}>.</button> <button className={style.btn}>AC</button>
-             <button className={style.btn}>+</button>
+            {
+                value.map((item)=><button className={style.btn}>{item}</button>
+
+                )
+            }
+            
         </div>
     )
 }
